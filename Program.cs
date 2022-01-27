ad4c9c7d959e709f7980e34ef501608e4558fc80
Laura warneke <Laurawarneke@usf.edu>
2022-01-27 00:40:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Created by Laura Warneke
+using System;
+
+namespace Tech_Assignment2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple, yet significant. Submitted by Laura Warneke");
+        }
+    }
+}
